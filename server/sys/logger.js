fd17d799983350c668d6ce5b7d1bb017aecd9f0c
alex sha <alltouch@ukr.net>
2013-12-04 01:12:26 +0200

    fix crashing

--- server/sys/logger.js
+++ server/sys/logger.js
@@ -47,7 +47,7 @@ var Logger = {
             console.log(process.cwd());
             console.log(__filename);
             console.log(__dirname);
-            var fd = fs.openSync("/app/server/." + file, "a+");
+            var fd = fs.openSync(file.replace('log/', 'tmp/'), "a+");
             fs.closeSync(fd);
         }
     },
